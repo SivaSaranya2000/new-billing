31d198fdf9d42971b625d03114a2ca9a24bb7991
Siva Saranya <sivafeather@gmail.com>
2026-04-06 17:27:35 +0530

    purchase1

--- database/migrations/2025_12_02_041431_create_purchases_table.php
+++ database/migrations/2025_12_02_041431_create_purchases_table.php
@@ -4,24 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+    
     public function up(): void
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
-            $table->decimal('unit_price', 10, 2)->nullable();
+            $table->unsignedBigInteger('supplier_id')->nullable();
+            $table->date('purchase_date')->nullable();
+            $table->string('purchase_invoice_no')->nullable();
+            $table->integer('qty')->default(0);
+            $table->decimal('mrp', 10, 2)->default(0);
+            $table->decimal('purchase_exc_tax', 10, 2)->default(0);
+            $table->decimal('purchase_inc_tax', 10, 2)->default(0);
+            $table->decimal('tax_amount', 10, 2)->default(0);
+            $table->decimal('unit_price', 10, 2)->default(0);
+            $table->decimal('price', 10, 2)->default(0);
+            $table->decimal('shipping_charges', 10, 2)->default(0);
+            $table->decimal('paid_amount', 10, 2)->default(0);
+            $table->string('payment_mode')->nullable();
+            $table->decimal('round_off', 10, 2)->default(0);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->decimal('total_amount', 12, 2)->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('purchases');
